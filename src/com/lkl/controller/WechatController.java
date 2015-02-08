cb0d394e4b8ada713b53253ddb206cd5a749aab6
likelei <656615066@163.com>
2015-02-09 00:31:23 +0800

    修改了webcharController实现

--- src/com/lkl/controller/WechatController.java
+++ src/com/lkl/controller/WechatController.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +26,6 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import com.lkl.domain.ReciveMessageRequest;
-import com.lkl.util.Decript;
 import com.lkl.util.WeChatContant;
 import com.qq.weixin.mp.aes.AesException;
 import com.qq.weixin.mp.aes.WXBizMsgCrypt;
@@ -39,18 +35,35 @@ import com.qq.weixin.mp.aes.WXBizMsgCrypt;
 public class WechatController {
 	private Logger logger = LoggerFactory.getLogger(WechatController.class);
 		
-	@RequestMapping("/")
-	public void index(Map<String,Object> context,
+	@RequestMapping(value = "/",method=RequestMethod.GET)
+	public void indexGet(Map<String,Object> context,
 			@RequestParam(required=false) String signature,
 			@RequestParam(required=false) String timestamp ,
 			@RequestParam(required=false) String nonce ,
 			@RequestParam(required=false) String echostr,
 			HttpServletRequest request,
 			HttpServletResponse response) throws Exception{
-		
-			String result = parseRequest(request) ;
+			
 			PrintWriter writer = response.getWriter();
-			writer.write(result);
+			writer.write(echostr);
+			writer.flush();
+			writer.close();
+				
+	}
+	
+	@RequestMapping(value = "/",method=RequestMethod.POST)
+	public void indexPost(Map<String,Object> context,
+			@RequestParam(required=false) String signature,
+			@RequestParam(required=false) String timestamp ,
+			@RequestParam(required=false) String nonce ,
+			@RequestParam(required=false) String echostr,
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception{
+			Object[] obj = parseXML(request);
+			//logger.info(result);
+			
+			PrintWriter writer = response.getWriter();
+			writer.write(obj[0].toString());
 			writer.flush();
 			writer.close();
 	}
@@ -67,6 +80,39 @@ public class WechatController {
 		String xml = sb.toString(); //次即为接收到微信端发送过来的xml数据}
 		return xml ;
 	}
+	
+	private Object[] parseXML(HttpServletRequest request) throws IOException, ParserConfigurationException, SAXException{
+		Object[] result = new Object[3];
+		
+		String xmltext = getReviceString(request) ;
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		StringReader sr = new StringReader(xmltext);
+		InputSource is = new InputSource(sr);
+		Document document = db.parse(is);
+
+		Element root = document.getDocumentElement();
+		NodeList nodelist1 = root.getElementsByTagName("ToUserName");
+		NodeList nodelist2 = root.getElementsByTagName("FromUserName");
+		NodeList nodelist3 = root.getElementsByTagName("CreateTime");
+		NodeList nodelist4 = root.getElementsByTagName("MsgType");
+		NodeList nodelist5 = root.getElementsByTagName("Content");
+		NodeList nodelist6 = root.getElementsByTagName("MsgId");
+		
+		logger.info("ToUserName"+nodelist1.item(0).getTextContent());
+		logger.info("FromUserName="+nodelist2.item(0).getTextContent());
+		logger.info("CreateTime="+nodelist3.item(0).getTextContent());
+		logger.info("MsgType="+nodelist4.item(0).getTextContent());
+		logger.info("Content="+nodelist5.item(0).getTextContent());
+		logger.info("MsgId="+nodelist6.item(0).getTextContent());
+		
+		result[0] = nodelist5.item(0).getTextContent();
+		result[2] = nodelist2.item(0).getTextContent();
+		return result;
+	}
+	
+	
 	private String parseRequest(HttpServletRequest request) throws AesException, IOException{
 		
 		// 需要加密的明文
